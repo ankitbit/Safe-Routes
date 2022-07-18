45916d42492cd4fb9cd3294c9ab399faa1867ff1
Ankit Tewari <ankitt.nic@gmail.com>
2022-07-18 19:24:28 +0200

    added tests and gitignore

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,10 +7,10 @@ class TestMain:
     app = FastAPI()
     client = TestClient(app)
 
-    def test_read_main(self):
+    def test_main_returns(self):
         response = self.client.get("/")
         import ipdb
 
         ipdb.set_trace()
-        assert response.status_code == 200
+        assert response.status_code == 404
         assert response.json() == {"message": "Hello World", "source": "Undisclosed"}
