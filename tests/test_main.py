365a1d0b0e17d3db00028d030326242d1f8a5414
Ankit Tewari <ankitt.nic@gmail.com>
2022-07-18 19:52:54 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,8 +9,4 @@ class TestMain:
 
     def test_main_returns(self):
         response = self.client.get("/")
-        import ipdb
-
-        ipdb.set_trace()
         assert response.status_code == 404
-        assert response.json() == {"message": "Hello World", "source": "Undisclosed"}
